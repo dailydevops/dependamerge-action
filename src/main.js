f21ffe0e589fa13d872cec9813a185421e45bc3e
Martin Stühmer <3283596+samtrion@users.noreply.github.com>
2023-12-21 21:36:22 +0100

    fix: try 3

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 const core = require('@actions/core')
+const github = require('@actions/github')
 
 const { logInfo, logDebug, logWarning } = require('./log')
 
@@ -6,18 +7,22 @@ const { logInfo, logDebug, logWarning } = require('./log')
  * The main function for the action.
  * @returns {Promise<void>} Resolves when the action is complete.
  */
-module.exports = async function run({ github, context, inputs, metadata }) {
+module.exports = async function run({ _, context, inputs, metadata }) {
   try {
-    logInfo(JSON.stringify(github))
-    logInfo(JSON.stringify(context))
-    logInfo(JSON.stringify(inputs))
+    logInfo('context: ', JSON.stringify(context))
+    logInfo('inputs: ', JSON.stringify(inputs))
 
     if (metadata !== undefined) {
-      logInfo(JSON.stringify(metadata))
+      logInfo('metadata: ', JSON.stringify(metadata))
     }
 
     // init octokit
-    const octokit = github.getOctokit(inputs.token)
+    const octokit = github.getOctokit(inputs.githubToken)
+    const pull_request = context.payload.pull_request
+    const repo = context.payload.repository
+
+    logInfo('pull_request: ', JSON.stringify(pull_request))
+    logInfo('repo: ', JSON.stringify(repo))
   } catch (error) {
     // Fail the workflow run if an error occurs
     core.setFailed(error.message)
