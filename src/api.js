8f5e790e310cd094946cb685db23f706139ac066
Martin Stühmer (CGI) <3283596+samtrion@users.noreply.github.com>
2024-01-10 23:27:07 +0100

    feat: Added `rebase` detection

--- src/api.js
+++ src/api.js
@@ -19,6 +19,14 @@ async function approvePullRequest(github, repo, pull_request, body) {
   })
 }
 
+async function comparePullRequest(github, repo, pull_request) {
+  return await github.rest.repos.compare({
+    owner: repo.owner.login,
+    repo: repo.name,
+    basehead: `${pull_request.base.ref}...${pull_request.head.ref}`
+  })
+}
+
 async function getPullRequest(github, repo, pull_request) {
   return await github.rest.pulls.get({
     owner: repo.owner.login,
@@ -30,5 +38,6 @@ async function getPullRequest(github, repo, pull_request) {
 module.exports = {
   addComment,
   approvePullRequest,
+  comparePullRequest,
   getPullRequest
 }
